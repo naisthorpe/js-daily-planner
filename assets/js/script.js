e570f312a51b5862d2983bebc4493a9af8efb0bd
naisthorpe <aisthorpe@gmail.com>
2021-02-27 01:01:23 -0500

    Store data locally and make it persist

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,6 @@ var hours = $(".hour");
 // Variable for current hour
 var currentHour = moment().format("HH");
 
-// currentHour = "14";
-
 // Add hours from planner to a list
 // That way I have something to iterate over
 for (var i=0; i<hours.length; i++) {
@@ -34,11 +32,11 @@ for (var i=0; i<timeBlock.length; i++) {
     timeBlockList.push(timeBlockText);
 }
 
+
 // Now do a for loop to compare the array of times to the current time
 // Change the class of the given id based on the time condition
 for (var i=0; i<timesList.length; i++) {
     var militaryTime = moment(timesList[i], "h A").format("HH");
-    console.log(militaryTime);
     if (militaryTime < currentHour) {
         $("#" + timeBlockList[i]).addClass("past");
     } else if (militaryTime === currentHour) {
@@ -47,4 +45,45 @@ for (var i=0; i<timesList.length; i++) {
         $("#" + timeBlockList[i]).addClass("future");
     }
 }
+
+
+// Okay, let's get to storing our data locally so it persists
+
+// Select the save buttons
+var saveBtn = $(".saveBtn");
+
+var inputStorageObject = {};
+
+// var inputText = $(this).parent().children("textarea").val();
+// var inputIdText = $(this).parent().children("textarea").attr("id");
+
+// Add event listener for save button
+// Store the text in a key:value pair >> key = id of textarea element  & value = text input
+saveBtn.on("click", function(event) {
+    event.preventDefault();
+    var inputText = $(this).parent().children("textarea").val();
+    var inputIdText = $(this).parent().children("textarea").attr("id");
+    inputStorageObject[inputIdText] = inputText;
+    localStorage.setItem("stored-text", JSON.stringify(inputStorageObject));
+});
+
+function init() {
+    var storedData = JSON.parse(localStorage.getItem("stored-text"));
+
+    if (storedData !== null) {
+        inputStorageObject = storedData;
+
+        var storedObject = Object.entries(inputStorageObject);
+
+        for (var i=0; i<storedObject.length; i++) {
+            var idInsert = storedObject[i][0];
+            var textInsert = storedObject[i][1];
+            $("#"+idInsert).text(textInsert);
+        }
+    }
+
+}
+
+
+init();
 
