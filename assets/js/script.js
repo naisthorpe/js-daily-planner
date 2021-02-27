a19442c9896f1659edd00314054125e7ce8fd93f
naisthorpe <aisthorpe@gmail.com>
2021-02-26 23:02:07 -0500

    Make timeblocks change color properly based on time comparison

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,50 @@
+// Call current date and set the text in the HTML
 var currentDate = moment().format("MMMM D, YYYY");
 $("#currentDay").text(currentDate);
 
-var relativeTime = $(".hour");
+// Empty array for times
+// Will be filled later
+var timesList = [];
 
-console.log(relativeTime);
+// Variable for each hour element
+var hours = $(".hour");
 
-var currentHour = moment().format("h A");
+// Variable for current hour
+var currentHour = moment().format("HH");
 
-var input = $("#input");
+// currentHour = "14";
 
-console.log(input);
+// Add hours from planner to a list
+// That way I have something to iterate over
+for (var i=0; i<hours.length; i++) {
+    var hourText = $(hours[i]).text();
+    timesList.push(hourText);
+}
 
-// if (relativeTime < currentHour) {
-//     input.attr("class", "past");
-// } else {
-//     console.log("fuk u");
-// }
+// Variable for each time block element
+var timeBlock = $(".time-block");
+
+// Empty array for textarea id's
+// Will use to assign class based on index
+var timeBlockList = [];
+
+// Add textarea id's to empty array for comparison later
+for (var i=0; i<timeBlock.length; i++) {
+    var timeBlockText = timeBlock[i].id;
+    timeBlockList.push(timeBlockText);
+}
+
+// Now do a for loop to compare the array of times to the current time
+// Change the class of the given id based on the time condition
+for (var i=0; i<timesList.length; i++) {
+    var militaryTime = moment(timesList[i], "h A").format("HH");
+    console.log(militaryTime);
+    if (militaryTime < currentHour) {
+        $("#" + timeBlockList[i]).addClass("past");
+    } else if (militaryTime === currentHour) {
+        $("#" + timeBlockList[i]).addClass("present");
+    } else {
+        $("#" + timeBlockList[i]).addClass("future");
+    }
+}
+
